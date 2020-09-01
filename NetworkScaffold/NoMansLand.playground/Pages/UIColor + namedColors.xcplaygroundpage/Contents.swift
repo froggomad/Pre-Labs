56944665cd5145ee3f9c4223fa4ac00fbd017882
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-09-01 14:05:39 -0700

    Added UIColor Extension

---
+++ NetworkScaffold/NoMansLand.playground/Pages/UIColor + namedColors.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: [Previous](@previous)
+
+import UIKit
+
+extension UIColor {
+    ///these colors are in XCAssets
+    enum ThemeColorName: String {
+        case backgroundLight
+        case backgroundDark
+        case textLight
+        case textDark
+        case actionLight
+        case actionDark
+        //etc
+    }
+
+    func getColor(_ named: ThemeColorName) -> UIColor {
+        UIColor(named: named.rawValue)
+    }
+
+    static var backgroundLight = getColor(.backgroundLight)
+    //etc
+}
+
+//: [Next](@next)

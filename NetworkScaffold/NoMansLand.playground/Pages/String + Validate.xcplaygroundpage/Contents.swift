658ea50bef4e2b8d73e54bd9bca0c3de05ad5007
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-09-01 07:03:48 -0700

    made sanitize a mutating func, changed validation methods not to be mutating

--- NetworkScaffold/NoMansLand.playground/Pages/String + Validate.xcplaygroundpage/Contents.swift
+++ NetworkScaffold/NoMansLand.playground/Pages/String + Validate.xcplaygroundpage/Contents.swift
@@ -1,9 +1,10 @@
 //: [Previous](@previous)
 
 //: String Validation
+import Foundation
 
 extension String {
-    func sanitize() {
+    mutating func sanitize() {
         //This will make an array split by punctuation
         let wordArray = self.split{ !$0.isLetter }
 
@@ -13,18 +14,19 @@ extension String {
 
     /// determine if a given string is a valid phone number
     func isValidPhone() -> Bool {
-        self.sanitize()
+        var string = self
+        string.sanitize()
         let phoneRegex = "^[0-9+]{0,1}+[0-9]{5,16}$"
         let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
-        return phoneTest.evaluate(with: self)
+        return phoneTest.evaluate(with: string)
     }
     /// determine if a given string is a valid email address
     func isValidEmail() -> Bool {
-        self.sanitize()
+        var string = self
+        string.sanitize()
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with: self)
+        return emailTest.evaluate(with: string)
     }
 }
-
 //: [Next](@next)
